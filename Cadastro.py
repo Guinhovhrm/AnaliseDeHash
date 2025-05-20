48d7f2f1bd5c060e82ac052fc3d9bde10b0780b6
guinhoPUC <macharetevic@gmail.com>
2025-05-19 21:57:07 -0300

    Parece_que_foi

--- Cadastro.py
+++ Cadastro.py
@@ -1,33 +1,43 @@
+#Victor Hugo Rodrigues Macharete
+#Caio Letty de Carvalho
+
 import hashlib
 
 login = True
-
 while login == True:
     n = int(input("O que você deseja fazer?\n1 - Cadastrar \n2 - Autenticar \n3 - Sair\n> "))
 
     if n == 1:
         print("cadastro")
-        usuario = input("Digite o nome do usuário")
-        senha = input("Digite a senha do usuário")
+        usuario = input("Digite o nome do usuário > ")
+        senha = input("Digite a senha do usuário > ")
         senha_md5 = hashlib.md5(senha.encode())
         senha_md5 = senha_md5.hexdigest()
-        with open("Credenciais.txt", "w") as arquivo:
+        with open("Credenciais.txt", "a") as arquivo:
             arquivo.write(f"{usuario},{senha_md5}\n")
         
     elif n == 2:
         print("autenticar")
         with open("Credenciais.txt", "r") as arquivo:
             conteudo = arquivo.readlines()
-
+        
+        user_verif = input(f"Digite o nome do usuário > ")
         for linha in conteudo:
             usuario = linha.split(",")
             nome_usuario = usuario[0]
             senha_usuario = usuario[1].replace("\n", "")
-            print(nome_usuario)
-            print(senha_usuario)
-
+            
+            if user_verif == nome_usuario:
+                senha_verif = input(f"Digite a senha de verificação do usuário {user_verif} > ")
+                hash_senha_verif = hashlib.md5(senha_verif.encode())
+                hash_senha_verif = hash_senha_verif.hexdigest()
+                    
+                if (hash_senha_verif == senha_usuario):
+                    print("A senha do usuário está correta")
+                else:
+                    print("A senha do usuário está incorreta")
+                    
     elif n == 3:
         login = False
-
-
-print("Você saiu")
+    
+    break
